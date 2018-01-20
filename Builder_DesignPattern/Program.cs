778b61d8adf7e6bd93d5b3e321e11e7c01b2fb45
BartMiki <bartmiki97@gmail.com>
2018-01-20 20:59:34 +0100

    CodeBuilder

--- Builder_DesignPattern/Program.cs
+++ Builder_DesignPattern/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Builder_DesignPattern
 {
+
+    public class CodeBuilder
+    {
+        private StringBuilder code;
+
+        public CodeBuilder(String className)
+        {
+            code = new StringBuilder();
+            code.Append($"public class {className}\n").Append("{\n");
+        }
+
+        public CodeBuilder AddField(String fieldName, String typeName)
+        {
+            code.Append($"  public {typeName} {fieldName};\n");
+            return this;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder(code.ToString());
+            result.Append("}");
+            return result.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
         }
     }
 }
