925b89424e063d02efc8720646724fb2c9302857
Bartosz Mikulski <bartmiki97@gmail.com>
2018-02-22 22:39:52 +0100

    Adapter Pattern added

---
+++ Adapter_DesignPattern/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_DesignPattern
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public SquareToRectangleAdapter(Square square)
+        {
+            Width = Height = square.Side;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square() { Side = 5 };
+            var adapter = new SquareToRectangleAdapter(square);
+            int area = adapter.Area();
+            Console.WriteLine($"Area of IRectangle is: {area}");
+        }
+    }
+}
---
+++ Singleton_DesignPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<string> str;
+            str = new Lazy<string>(() => "Singleton");
+
+            str.
+        }
+    }
+}
